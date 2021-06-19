c275206fd20889b1ec307936640ee9a38b9a2cf2
prakharsam <prakharsam97@gmail.com>
2021-06-18 18:42:05 -0700

    Created tetris blocks

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 using namespace std;
 
+wstring blocks[7];
+
 int main() {
-    cout << "Hello, World!" << endl;
+
+    //tetris blocks
+    blocks[0].append(L"..X...X...X...X.");
+    blocks[1].append(L"..X..XX...X.....");
+    blocks[2].append(L".....XX..XX.....");
+    blocks[3].append(L"..X..XX..X......");
+    blocks[4].append(L".X...XX...X.....");
+    blocks[5].append(L".X...X...XX.....");
+    blocks[6].append(L"..X...X..XX.....");
+
     return 0;
 }
