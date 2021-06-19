410fe97215a2881e025b569c7ce5b23de184ac65
prakharsam <prakharsam97@gmail.com>
2021-06-18 18:45:41 -0700

    Initialized playing field

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 using namespace std;
 
+int nFieldWidth = 12;
+int nFieldHeight = 18;
+unsigned char *pField = nullptr;
 wstring blocks[7];
 
 int main() {
@@ -13,6 +16,11 @@ int main() {
     blocks[4].append(L".X...XX...X.....");
     blocks[5].append(L".X...X...XX.....");
     blocks[6].append(L"..X...X..XX.....");
+
+    pField = new unsigned char[nFieldWidth*nFieldHeight]; // Create play field buffer
+    for (int x = 0; x < nFieldWidth; x++) // Board Boundary
+        for (int y = 0; y < nFieldHeight; y++)
+            pField[y*nFieldWidth + x] = (x == 0 || x == nFieldWidth - 1 || y == nFieldHeight - 1) ? 9 : 0;
 
     return 0;
 }
